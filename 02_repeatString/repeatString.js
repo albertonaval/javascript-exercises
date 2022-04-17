fd26bddadbb4f412bd6374f427614e9e1b08f7d8
Alberto Naval <albertonaval.dev@gmail.com>
2022-04-17 13:39:07 +0100

    Add 01/04 excercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
 
-};
+const repeatString = function (string, times) {
+   if (times > 0) {
+      return string.repeat(times);
+   } else if (times < 0) {
+      return "ERROR"; // Error the number cant be negative.
+   } else {
+      return "";
+   }
+}
 
+// Entre parentesis argumentos de la funcion.
 // Do not edit below this line
 module.exports = repeatString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
 
-};
+const removeFromArray = function (array, ...items) {
+
+   return array.filter(i => !items.includes(i));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
