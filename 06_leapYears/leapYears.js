0115ec00d0e59fa73cc4dd45467000e3cfbcbdda
Alberto Naval <albertonaval.dev@gmail.com>
2022-04-20 20:41:35 +0100

    Add leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+// Numeros divididos entre 4 or 400 = True.
+// Numeros divididos entre 4 y 100 y 400 = True
+// Lo demas falso.
 
-};
+
+
+
+const leapYears = function (year) {
+   let divisibleBy4 = (year % 4) === 0;
+   let divisibleBy400 = (year % 400) === 0;
+   let divisibleBy100 = (year % 100) === 0;
+
+   if (divisibleBy4 && divisibleBy100 && divisibleBy400) { return true }
+   else if(divisibleBy4 && divisibleBy100) {return false}
+   else {return false}
+
+}
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
