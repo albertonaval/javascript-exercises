e33e462497bc29dfc2bc4b7b853d49851b8ce0d7
Alberto Naval <albertonaval.dev@gmail.com>
2022-04-23 18:15:25 +0100

    Add tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+// Tengo que crear un conversor de medidas de tempertura con dos variables:
+  // Farenheit = C * 9/5 + 32;
+  // Celsius = (F - 32º)  * 5/9;
+  // Para el redonde es mejor aplicar Math.round ya que toFixed() devuelve un string y no queremos un string.
+  // Math.round(temperatura que quiero retornar * 10) / 10; Me devuelve un decimal.
+// El argumento que le paso a la function es la temperatura que introduzco.
 
+const ftoc = function (fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9
+  return Math.round(celsius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  const fahrenheit = celsius * 9 / 5 + 32
+  return Math.round(fahrenheit * 10) /10;
 };
 
 // Do not edit below this line
